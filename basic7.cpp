4d00c4a1d153aed1ba292ad4a1f36002ae3bbcfc
Junayed Rahaman <35622638+junayedrahaman50@users.noreply.github.com>
2024-02-21 00:10:49 +0530

    new cpp files

--- basic7.cpp
+++ basic7.cpp
@@ -24,5 +24,83 @@ int main()
     {
         std::cout << "You are not old enough to enter!";
     }
+
+    /* switch: alternative to using many else if statements compare one value against matching cases  */
+
+    int month;
+    std::cout << "\nEnter the month(1-12): ";
+    std::cin >> month;
+
+    switch (month)
+    {
+    //   case value:
+    case 1:
+        std::cout << "It is January";
+        break;
+    case 2:
+        std::cout << "It is February";
+        break;
+    case 3:
+        std::cout << "It is March";
+        break;
+    case 4:
+        std::cout << "It is April";
+        break;
+    case 5:
+        std::cout << "It is May";
+        break;
+    case 6:
+        std::cout << "It is June";
+        break;
+    case 7:
+        std::cout << "It is July";
+        break;
+    case 8:
+        std::cout << "It is August";
+        break;
+    case 9:
+        std::cout << "It is September";
+        break;
+    case 10:
+        std::cout << "It is October";
+        break;
+    case 11:
+        std::cout << "It is November";
+        break;
+    case 12:
+        std::cout << "It is December";
+        break;
+        // for no matching cases execute default case (kind of like else statement)
+    default:
+        std::cout << "Please enter a value between (1-12)";
+    }
+
+    std::cout << '\n';
+    char grade;
+
+    std::cout << "What letter grade?: ";
+    std::cin >> grade;
+
+    switch (grade)
+    {
+    case 'A':
+        std::cout << "You did great!";
+        break;
+    case 'B':
+        std::cout << "You did good!";
+        break;
+    case 'C':
+        std::cout << "You did okay!";
+        break;
+    case 'D':
+        std::cout << "You did not do good!";
+        break;
+    case 'F':
+        std::cout << "You failed!";
+        break;
+    default:
+        std::cout << "Please enter a letter grade (A-F)";
+    }
+
     return 0;
 }
